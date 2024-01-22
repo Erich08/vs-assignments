89955fc0e740d87c7f4b2c699129e9979d8c49cf
Erich Hartnauer <erich.hartnauer@gmail.com>
2024-01-22 09:34:26 -0600

    completed assignment

--- exercises/level-one/dom-debug/index.js
+++ exercises/level-one/dom-debug/index.js
@@ -1,32 +1,35 @@
-const colors = ["red", "blue", "green"]
+const colors = ['red', 'blue', 'green'];
 
-document.getElementById("add").addEventListener("onclick", function(e){
-    const subItem = createSubItem(e)
-    document.getElementById("list").appendChild(subItem)
-})
+//Changed 'onclick' to 'click.'
+document.getElementById('add').addEventListener('click', function (e) {
+  const subItem = createSubItem(e);
+  document.getElementById('list').appendChild(subItem);
+});
 
-function createDropDown(){
-    const dropDown = document.createElement("select")
-    for (let i = 0; i < colors; i++){
-        const option = createElement("option") 
-        option.innerHTML = colors[i]
-        option.value = colors[i]
-        dropDown.append(option)
-    }
-    dropDown.addEventListener("onchange", function(e){
-        e.target.parent.backgroundColor = e.target.value
-    })
-    return dropDown
+//Line 13 added 'document.createElement // document. was missing. //Line 12 added 'colors.length' length was originally missing.
+function createDropDown() {
+  const dropDown = document.createElement('select');
+  for (let i = 0; i < colors.length; i++) {
+    const option = document.createElement('option');
+    option.innerHTML = colors[i];
+    option.value = colors[i];
+    dropDown.append(option);
+  }
+
+  //Line 21 added 'parentElement' and '.style.backgroundColor' //Also changed 'onchange' to 'change.'
+  dropDown.addEventListener('change', function (e) {
+    e.target.parentElement.style.backgroundColor = e.target.value;
+  });
+  return dropDown;
 }
 
-function createSubItem(e){
-    const subItem = document.createElement("div")
-    var subItemValue = document.getElementById("input")
-    subItem.textContent = subItemValue
-    const dropDown = createDropDown()
-    subItem.appendChild(dropDown)
-    subItem.setAttribute("class", "subItem")
-    return subItem
+function createSubItem(e) {
+  const subItem = document.createElement('div');
+  var subItemValue = document.getElementById('input');
+  subItem.textContent = subItemValue.value;
+  subItemValue.value = '';
+  const dropDown = createDropDown();
+  subItem.appendChild(dropDown);
+  subItem.setAttribute('class', 'subItem');
+  return subItem;
 }
-
-
